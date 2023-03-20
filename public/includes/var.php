b3e94bf70837d1b1544237b53dd1517a3c607068
Christopher <christo26.g@gmail.com>
2023-03-20 19:47:13 +0100

    localhost check

--- public/includes/var.php
+++ public/includes/var.php
@@ -1,4 +1,9 @@
 <?php 
-//$domain = "http://localhost/Segr%c3%a9Conduite/public/";
-$domain =  "https://kekoeur.github.io/Segre-Conduite/public/"
+
+if ($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == '127.0.0.1') {
+    $domain = "http://localhost/Segr%c3%a9Conduite/public/";
+  } else {
+    $domain =  "https://kekoeur.github.io/Segre-Conduite/public/";
+  }
+
 ?>
