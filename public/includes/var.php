f02543dd09e694fb0ceeced0a5679010c1b217a1
Christopher <christo26.g@gmail.com>
2023-03-20 19:45:11 +0100

    files

--- public/includes/var.php
+++
@@ -1,4 +0,0 @@
-<?php 
-//$domain = "http://localhost/Segr%c3%a9Conduite/public/";
-$domain =  "https://kekoeur.github.io/Segre-Conduite/public/"
-?>
---
+++ public/pages/permis-B.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once($_SERVER['DOCUMENT_ROOT'].'/SegréConduite/public/includes/var.php');
+$pageName = "Permis B";
+require_once (__DIR__ . '/../includes/header.php');
+require_once(__DIR__.'/../includes/page_header.php');
+
+?>
+<h1>Permis B</h1>
